4f10d08a6d7000549b905dafc995631aad2e5990
HyunJungJo98 <kchwoonn0827@naver.com>
2023-04-17 17:01:02 +0900

    230417 : 13023

--- 백트래킹/13023 - ABCDE.js
+++ 백트래킹/13023 - ABCDE.js
@@ -4,57 +4,57 @@ const input = fs
   .toString()
   .trim()
   .split('\r\n');
-// const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+// dev 앞에 /안 붙일 시 런타임 에러(TypeError)
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
 // 2차원 배열에 저장하지 말고 객체에 저장해보기
-const friends = Array.from(Array(n), () => Array(n).fill(0));
-const visited = Array.from(Array(n), () => Array(n).fill(0));
+const friends = {};
+// const friends = Array.from(Array(n), () => Array(n).fill(0));
+// const visited = Array.from(Array(n), () => Array(n).fill(0));
 for (let i = 1; i < m + 1; i++) {
   const [a, b] = input[i].split(' ');
-  friends[+a][+b] = 1;
-  friends[+b][+a] = 1;
+  if (friends[a]) {
+    friends[a].push(b);
+  } else {
+    friends[a] = [b];
+  }
+  if (friends[b]) {
+    friends[b].push(a);
+  } else {
+    friends[b] = [a];
+  }
 }
 
 const relation = [];
 let isExist = 0;
 
 const dfs = (row) => {
+  console.log('row', row, relation);
   if (relation.length === 5 || isExist === 1) {
     isExist = 1;
     return;
   }
 
-  for (let i = 0; i < n; i++) {
-    if (
-      friends[row][i] === 1 &&
-      visited[row][i] === 0 &&
-      visited[i][row] === 0
-    ) {
-      relation.push(i);
-      visited[row][i] = 1;
-      visited[i][row] = 1;
-      dfs(i);
-      visited[row][i] = 0;
-      visited[i][row] = 0;
+  for (let i = 0; i < friends[row].length; i++) {
+    if (!relation.includes(friends[row][i])) {
+      console.log('?', friends[row][i]);
+      relation.push(friends[row][i]);
+      dfs(friends[row][i]);
       relation.pop();
     }
   }
 };
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (friends[i][j] === 1 && isExist === 0) {
-      relation.push(i);
-      relation.push(j);
-      visited[i][j] = 1;
-      visited[j][i] = 1;
-      dfs(j);
-      visited[i][j] = 0;
-      visited[j][i] = 0;
-      relation.pop();
-      relation.pop();
-    }
+console.log(friends);
+
+for (let key in friends) {
+  for (let i = 0; i < friends[key].length; i++) {
+    relation.push(key);
+    relation.push(friends[key][i]);
+    dfs(friends[key][i]);
+    relation.pop();
+    relation.pop();
   }
 }
 
