4501621158758dc6e3824286bdf99bad3b8bb292
HyunJungJo98 <kchwoonn0827@naver.com>
2023-09-07 17:19:13 +0900

    230907 : 숫자 게임, 2 x n 타일링, 단속카메라

--- Summer_Winter_Coding(~2018)/숫자 게임.js
+++ Summer_Winter_Coding(~2018)/숫자 게임.js
@@ -1,15 +1,16 @@
 function solution(A, B) {
-  let answer = 0;
   const n = A.length;
-  const a = A.sort((a, b) => b - a);
-  const b = B.sort((a, b) => b - a);
+  const a = A.sort((a, b) => a - b);
+  const b = B.sort((a, b) => a - b);
+  let index = 0;
 
-  for (let i = 0; i < n; i++) {
-    if (a[i] < b[i]) {
-      answer += 1;
+  for (let i = 0; i < n && index < n; i++) {
+    if (a[index] < b[i]) {
+      index++;
     }
   }
-  return answer;
+
+  return index;
 }
 
 console.log(solution([5, 1, 3, 7], [2, 2, 6, 8]));
